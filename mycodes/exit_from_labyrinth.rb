d15b0ad2419500099353de87c1f76dbe34174065
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-15 11:38:55 +0200

    fix method name

--- mycodes/exit_from_labyrinth.rb
+++ mycodes/exit_from_labyrinth.rb
@@ -11,12 +11,12 @@ while pos_x != 2 and pos_y != 6
     elsif is_clear_left()
         turn_left()
     else
-        turn_back()
+        turn_around()
     end
 end
 say("you took "+step_counter+" steps")
 
-def turn_back()
+def turn_around()
     turn_right()
     turn_right()
 end

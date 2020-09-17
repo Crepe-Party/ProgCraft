b8ad3a80669308779dbdcff591441d9e7b9961ac
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-17 08:57:57 +0200

    fix rectangle class

--- core/ui_elements/drawables/rectangle.rb
+++ core/ui_elements/drawables/rectangle.rb
@@ -1,7 +1,7 @@
 # rectangle
 class Rectangle
     attr_accessor :x, :y, :width, :height, :color
-    def initialize x, y, width, height, color
+    def initialize x, y, width, height, color=Gosu::Color::BLACK
         @x, @y, @width, @height, @color = x, y, width, height, color
     end
     def draw

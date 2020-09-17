ae4bbf0b454f20505d2f24cdf03be5204c2166e4
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-17 08:39:40 +0200

    add triangle class

--- core/ui_elements/drawables/triangle.rb
+++ core/ui_elements/drawables/triangle.rb
@@ -1,1 +1,10 @@
 # triangle
+class Triangle
+    attr_accessor :x, :y, :width, :height, :color, :angle
+    def initialize x, y, width, height, color, angle=0
+        @x, @y, @width, @height, @color, @angle = x, y, width, height, color, angle
+    end
+    def draw
+        Gosu.draw_triangle(@x, @y, @color, @x+@width, @y, @color, @x+(@width/2), @y+@height, @color)
+    end
+end

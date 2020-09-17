e3099b8d698b1f677d0efcbe6def6a67c6381a98
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-17 09:06:07 +0200

    fix triangle class

--- core/ui_elements/drawables/triangle.rb
+++ core/ui_elements/drawables/triangle.rb
@@ -1,6 +1,6 @@
 # triangle
 class Triangle
-    attr_accessor :x, :y, :width, :height, :color, :angle
+    attr_accessor :x1, :y1, :x2, :y2, :x3, :y3, :color, :angle
     def initialize x1, y1, x2, y2, x3, y3, color=Gosu::Color::BLACK, angle=0
         @x1, @y1, @x2, @y2, @x3, @y3, @color, @angle = x1, y1, x2, y2, x3, y3, color, angle
     end

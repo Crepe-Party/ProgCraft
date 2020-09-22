8c5e391e643b8b3af6997f5551a8ffcf485bb388
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-22 11:56:47 +0200

    fix vector class

--- core/map.rb
+++ core/map.rb
@@ -1,14 +1,20 @@
+require_relative './tools/vector'
 class Map
     attr_accessor :name, :player_spawn, :player_inventory, :elements
     def initialize
-
+        @name = ''
+        @player_spawn = Vector2.new
+        @player_inventory = Array.new
+        @elements = Array.new
     end
     def load map
         @name = map['name']
-        @player_spawn = Vector2.new(map['player_spawn']['position']['x'], map['player_spawn']['position']['y'])
-        map['player_spawn']['inventory'].each |item|
-            puts item
+        @player_spawn.x = map['player_spawn']['position']['x']
+        @player_spawn.y = map['player_spawn']['position']['y']
+        map['player_spawn']['inventory'].each do |item|
+            element = item['object']['type'].camelize.constantize.new
+            @elements << element
         end
-    # Object.const_get('ExampleClass')
+        puts @elements
     end
 end

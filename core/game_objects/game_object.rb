53888d2f27f5ae10cd249f2cd34e31f5d1ded1e7
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-23 17:50:06 +0200

    fix capitalize convert string from map to class

--- core/game_objects/game_object.rb
+++ core/game_objects/game_object.rb
@@ -1,6 +1,6 @@
 class Game_object
     attr :position, :texture, :name, :id
-    def initialize name, id, texture='basic_texture.png', position=Vector2.new(0,0)
+    def initialize name=nil, id=nil, texture='basic_texture.png', position=Vector2.new(0,0)
         @name, @id, @name, @texture = name, id, texture, position 
     end 
 end
--- core/map.rb
+++ core/map.rb
@@ -15,14 +15,12 @@ class Map
         @player_spawn.x = map['player_spawn']['position']['x']
         @player_spawn.y = map['player_spawn']['position']['y']
         map['player_spawn']['inventory'].each do |item|
-            object = Object.const_get(item['object']['type']).new
+            object = Object.const_get(item['object']['type'].capitalize).new
             @player_inventory << object
         end
         map['elements'].each do |item|
-            element = Object.const_get(item['type']).new
+            element = Object.const_get(item['type'].capitalize).new
             @elements << element
         end
-        puts @player_inventory
-        puts @elements
     end
 end

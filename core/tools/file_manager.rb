98f7b33c7aa1aa80be37c61a3d0a16325de146e9
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-29 14:01:52 +0200

    add write method

--- core/tools/file_manager.rb
+++ core/tools/file_manager.rb
@@ -4,4 +4,9 @@ module File_manager
     def self.read_level_file path_file
         return File.exist?(path_file) ? JSON.parse(File.read(path_file)) : nil
     end
+    def self.write_level_file path_file, json
+        File.open(path_file,"w") do |file|
+            file.write(json.to_json)
+        end
+    end
 end

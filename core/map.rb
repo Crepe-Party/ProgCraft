dea917930344d87604cccaa08b0f9ebeec78707d
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-29 10:13:28 +0200

    move game objects into GameObjects module

--- core/map.rb
+++ core/map.rb
@@ -12,16 +12,15 @@ class Map
     end
     def load map
         @name = map['name']
-        @player_spawn.x = map['player_spawn']['position']['x']
-        @player_spawn.y = map['player_spawn']['position']['y']
-        map['player_spawn']['inventory'].each do |item|
-            object = Object.const_get(item['object']['type'].capitalize).new
+        @player_spawn.x = map['robert']['position']['x']
+        @player_spawn.y = map['robert']['position']['y']
+        map['robert']['inventory'].each do |item|
+            object = GameObjects.const_get(item['object']['type'].capitalize).new
             @player_inventory << object
         end
         map['elements'].each do |item|
-            # classs = Object.const_get(item['type'].capitalize)
-            clazz = Object.const_get(item['type'].capitalize)
-            element = clazz.new item['id'], item['name'], __dir__+'/assets/'+item['data']['texture'], Vector2.new(item['position']['x'], item['position']['y'])
+            class_GameObject = GameObjects.const_get(item['type'].capitalize)
+            element = class_GameObject.new item['id'], item['name'], __dir__+'/assets/'+item['data']['texture'], Vector2.new(item['position']['x'], item['position']['y'])
             @elements << element
         end
     end

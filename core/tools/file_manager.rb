6ae69eebfe20c7bbd7ab4ca1bd34d914b29f1aef
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-10-01 10:19:40 +0200

    change name file read and write method

--- core/tools/file_manager.rb
+++ core/tools/file_manager.rb
@@ -1,10 +1,10 @@
 require 'singleton'
 require 'json'
 module File_manager
-    def self.read_level_file path_file
+    def self.read_json path_file
         return File.exist?(path_file) ? JSON.parse(File.read(path_file)) : nil
     end
-    def self.write_level_file path_file, json
+    def self.write_json path_file, json
         if File.exist?(path_file)
             File.open(path_file,"w") do |file|
                 file.write(json.to_json)

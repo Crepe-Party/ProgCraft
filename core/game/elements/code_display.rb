a2910f07972eecd1ffd3ea48e9590df7b9288af4
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-10-08 10:23:26 +0200

    change display color

--- core/game/elements/code_display.rb
+++ core/game/elements/code_display.rb
@@ -5,13 +5,13 @@ class CodeDisplay < Scrollable
     attr :code
     LINE_NUMBER_WIDTH = 40
     def build
-        self.background_color = Gosu::Color.rgba(235,235,235,255)
+        self.background_color = Gosu::Color.rgba(0,0,0,0)
         lines_number = ""
         1.upto 50 do |line_number|
             lines_number+="#{line_number}\n"
         end
-        @sub_elements[:line_number] = Text.new(@root, lines_number, center_text: false){@scrl_rect.assign(width: LINE_NUMBER_WIDTH)}
-        @sub_elements[:code] = Text.new(@root, "", center_text: false){@scrl_rect.relative_to(x: LINE_NUMBER_WIDTH, width: -LINE_NUMBER_WIDTH)}
+        @sub_elements[:line_number] = Text.new(@root, lines_number, center_text: false, color: Gosu::Color.rgba(200,200,200,255)){@scrl_rect.assign(width: LINE_NUMBER_WIDTH)}
+        @sub_elements[:code] = Text.new(@root, "", center_text: false, color: Gosu::Color::WHITE){@scrl_rect.relative_to(x: LINE_NUMBER_WIDTH, width: -LINE_NUMBER_WIDTH)}
         super
     end
     def vertical?
@@ -20,5 +20,7 @@ class CodeDisplay < Scrollable
     def load path_file
         @code = File_manager.read path_file
         @sub_elements[:code].string = @code
+        #windows
+        # system("start #{path_file}")
     end
 end

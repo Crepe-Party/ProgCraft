d9eee544bd517da671a1a045744bcc95822cb858
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-11-04 07:46:32 +0100

    add image to button

--- core/game/elements/top_bar.rb
+++ core/game/elements/top_bar.rb
@@ -5,6 +5,33 @@ class GameTopBar < UIElement
     MAPS_DIR_CODES = File.join(File.dirname(__FILE__), '../../../mycodes/')
     def build
         self.background_color = Gosu::Color::GRAY
+        @sub_elements[:play_button] = Button.new(@root, "", background_image: 'icons/forward.png', background_image_cover: true)
+            .constrain{Rectangle2.new(@rectangle.x + 5, @rectangle.y + 5, 40, 40)}
+            .add_event(:mouse_down, options = {button: Gosu::MS_LEFT}){
+                @root.busy = true
+                @root.window_manager.open_file(MAPS_DIR_MAP) do |path_file| 
+                    @root.load_map path_file
+                    @root.busy = false
+                end
+            }
+        @sub_elements[:pause_button] = Button.new(@root, "", background_image: 'icons/pause.png', background_image_cover: true)
+            .constrain{Rectangle2.new(@rectangle.x + 50, @rectangle.y + 5, 40, 40)}
+            .add_event(:mouse_down, options = {button: Gosu::MS_LEFT}){
+                @root.busy = true
+                @root.window_manager.open_file(MAPS_DIR_MAP) do |path_file| 
+                    @root.load_map path_file
+                    @root.busy = false
+                end
+            }
+        @sub_elements[:next_button] = Button.new(@root, "", background_image: 'icons/next.png', background_image_cover: true)
+            .constrain{Rectangle2.new(@rectangle.x + 95, @rectangle.y + 5, 40, 40)}
+            .add_event(:mouse_down, options = {button: Gosu::MS_LEFT}){
+                @root.busy = true
+                @root.window_manager.open_file(MAPS_DIR_MAP) do |path_file| 
+                    @root.load_map path_file
+                    @root.busy = false
+                end
+            }
         @sub_elements[:load_map_button] = Button.new(@root, "Load Map")
             .constrain{Rectangle2.new(@rectangle.right - 300 - 15, @rectangle.y + 5, 150, 40)}
             .add_event(:mouse_down, options = {button: Gosu::MS_LEFT}){
--- core/ui_elements/drawables/image.rb
+++ core/ui_elements/drawables/image.rb
@@ -1,12 +1,15 @@
 require_relative 'drawable'
 class Image < Drawable
     attr_reader :src
-    def initialize root, source= nil, &contraint
+    def initialize root, source= nil, cover: false, &contraint
         super root, &contraint
         self.source = source if source
+        @cover = cover
     end
     def draw
-        @image.draw(@rectangle.x, @rectangle.y, 0) if @image
+        scale_y = @cover ? @rectangle.height/@image.height : 1
+        scale_x = @cover ? @rectangle.width/@image.width : 1
+        @image.draw(@rectangle.x + @rectangle.width/2 - @image.width*scale_x/2, @rectangle.y + @rectangle.height/2 - @image.height*scale_y/2, 0, scale_x, scale_y) if @image
     end
     def source= src
         @source = src

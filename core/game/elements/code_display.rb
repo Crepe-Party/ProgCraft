2223b938f4647bf08c8a820246d7d488ddf05e37
Florian Bergmann <flobergmann@aol.ch>
2020-11-17 10:38:47 +0100

    add manage program file delete and load new program

--- core/game/elements/code_display.rb
+++ core/game/elements/code_display.rb
@@ -2,11 +2,14 @@ require_relative '../../ui_elements/widgets/scrollable'
 require_relative '../../ui_elements/drawables/text'
 require_relative '../../tools/file_manager'
 class CodeDisplay < Scrollable
-    attr :code
+    attr :code, :mtime
     LINE_HEIGHT = 20
+    DEFAULT_PATH_FILE = File.expand_path("../../../mycodes/sans_titre.rb", File.dirname(__FILE__))
     def build
         self.background_color = Gosu::Color.rgba(0,0,0,255)
         @code_lines_text_keys = []
+        @path_file = DEFAULT_PATH_FILE
+        puts @path_file
         sync
         super
     end
@@ -16,8 +19,7 @@ class CodeDisplay < Scrollable
     def load path_file
         @path_file = path_file
         @mtime = File.mtime @path_file
-        @sub_elements.reject!{|key,val|@code_lines_text_keys.include? key}
-        @code_lines_text_keys.clear
+        clear_code
         @code = File_manager.read @path_file
         code_lines = @code.split("\n")
         code_lines.each_with_index do |code_line, index|
@@ -28,17 +30,16 @@ class CodeDisplay < Scrollable
         end
         apply_constraints
     end
+    def clear_code
+        @sub_elements.reject!{|key,val|@code_lines_text_keys.include? key}
+        @code_lines_text_keys.clear
+    end
     def sync
         Thread.new{
             loop do
-                if @mtime && @path_file
-                    if @mtime != (File.mtime @path_file)
-                        mtime = File.mtime @path_file
-                        load @path_file
-                    end
-                end
+                (File.exist? @path_file) ? (load @path_file if @mtime != (File.mtime @path_file)) : clear_code
                 sleep 1
             end
         }
-    end 
+    end
 end
--- core/game/elements/code_menu.rb
+++ core/game/elements/code_menu.rb
@@ -7,7 +7,7 @@ class CodeMenu < UIElement
     def build
         self.background_color = Gosu::Color.rgba(200,200,200,255)
         @path_file = "../../../mycodes/sans_titre.rb"
-        @sub_elements[:filename] = Text.new(@root, "programe_sans_titre.rb", center_text: false, color: Gosu::Color::BLACK, font: Gosu::Font.new(20 ,name: "Consolas")){@rectangle.relative_to(x: 5 ,y: @rectangle.y + @rectangle.height/2 - 10)}
+        @sub_elements[:filename] = Text.new(@root, "sans_titre.rb", center_text: false, color: Gosu::Color::BLACK, font: Gosu::Font.new(20 ,name: "Consolas")){@rectangle.relative_to(x: 5 ,y: @rectangle.y + @rectangle.height/2 - 10)}
         @sub_elements[:load_program_button] = Button.new(@root, "Edit")
         .constrain{Rectangle2.new(@rectangle.right - BUTTON_WIDTH - 5, @rectangle.y + 5, BUTTON_WIDTH, @rectangle.height - 10)}
         .add_event(:mouse_down, options = {button: Gosu::MS_LEFT}){

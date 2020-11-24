96a3002134e13677f380379abdac64f42626dd3e
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-11-24 09:41:59 +0100

    add display robert on game map

--- core/game/game_manager.rb
+++ core/game/game_manager.rb
@@ -2,14 +2,15 @@ require 'pp'
 require_relative '../app_manager'
 require_relative 'elements/game_ui'
 require_relative '../level'
-require_relative '../game_objects/player'
+require_relative '../player'
 class GameManager < AppManager
     attr :level, :player
     def initialize window
         super window, main_ui_class: GameUI
         @busy_string = "Loading..."
         @level = Level.new
-        @player = Player.new(600, 300)
+        @player = Player.new(0, 0)
+        @main_ui.sub_elements[:map_game].player = @player
     end
     def play
     
@@ -24,8 +25,8 @@ class GameManager < AppManager
         @level_available = @level.load path_file
         unless @level_available.nil?
             @main_ui.sub_elements[:map_name].path_file = path_file
-            @main_ui.sub_elements[:map_game].selected_map = @level.maps[0] 
-            @player.set_pos @level.maps[0].robert_spawn.x, @level.maps[0].robert_spawn.y
+            @main_ui.sub_elements[:map_game].selected_map = @level.maps[0]
+            # @player.set_pos @level.maps[0].robert_spawn.x, @level.maps[0].robert_spawn.y
         end
     end
     def load_program path_file
---
+++ core/player.rb
@@ -0,0 +1,17 @@
+require_relative 'ui_elements/drawables/sprite'
+require_relative 'ui_elements/ui_element'
+require_relative 'tools/vector'
+class Player < UIElement
+    attr :position, :texture
+    def initialize x=0, y=0
+        @position = Vector2.new(x,y)
+        @texture = Gosu::Image.new(__dir__+'/assets/robert_test.png')
+    end
+    def set_pos x, y
+        @position.x = x
+        @position.y = y
+    end
+    def draw posX, posY
+        @texture.draw(posX, posY, 0)
+    end
+end

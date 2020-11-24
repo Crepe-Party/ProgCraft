97d17f95d10607955a32e703bdfd6a060934b924
DiogoVF <diogof-6@hotmail.com>
2020-11-24 22:10:44 +0100

    fix dirname error

--- core/config.rb
+++ core/config.rb
@@ -1,5 +1,5 @@
 module Config
-    @@base_path = defined?(Ocra) ? File.dirname(ENV["OCRA_EXECUTABLE"]) : File.dirname(__FILE__)    
+    @@base_path = ENV["OCRA_EXECUTABLE"] != nil ? File.dirname(ENV["OCRA_EXECUTABLE"]) : File.dirname(__FILE__)    
 
     ASSETS_DIR = File.join(@@base_path, "assets")
     MY_CODES_DIR = File.join(@@base_path, "../mycodes")

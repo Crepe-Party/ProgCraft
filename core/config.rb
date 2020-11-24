255470a2bd090d71978611879d82f5140c530c95
Florian Bergmann <flobergmann@aol.ch>
2020-11-24 22:43:55 +0100

    special florian fix!

--- core/config.rb
+++ core/config.rb
@@ -1,5 +1,5 @@
 module Config
-    @@base_path = ENV["OCRA_EXECUTABLE"] != nil ? File.dirname(ENV["OCRA_EXECUTABLE"]) : File.dirname(__FILE__)    
+    @@base_path = ENV["OCRA_EXECUTABLE"] != nil ? File.join(File.dirname(ENV["OCRA_EXECUTABLE"]),"core") : File.dirname(__FILE__)    
 
     ASSETS_DIR = File.join(@@base_path, "assets")
     MY_CODES_DIR = File.join(@@base_path, "../mycodes")

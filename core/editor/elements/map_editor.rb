392ad12349c194a69d252fbb7cdd33239a6c5cc9
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-11-26 10:21:55 +0100

    place objects on map

--- core/editor/elements/map_editor.rb
+++ core/editor/elements/map_editor.rb
@@ -8,6 +8,26 @@ class MapEditorDisplay < GridGameContainer
             clicked_elem = game_object_at_grid_position(projected_grid_position(event[:position]))
             @selected_map.game_objects.delete(clicked_elem) if clicked_elem
         end
+        self.add_event(:mouse_down, button: Gosu::MS_LEFT) do |event|
+            grid_pos = projected_grid_position(event[:position])
+            p grid_pos
+            #select object
+            if(obj = game_object_at_grid_position(grid_pos))
+                puts "already an object here"
+                select_object obj
+                next
+            end
+            #add object
+            unless @root.selected_object_type
+                puts "no object type selected"
+                next
+            end
+            new_obj = @root.selected_object_type.new(position: grid_pos)
+            @selected_map.game_objects.push(new_obj)
+        end
+    end
+    def select_object object
+        puts "selecting a #{object.class.to_s}"
     end
     def zoomable?
         false

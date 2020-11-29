5f282a16133b53686b22b0cf2a236e22e4a4615c
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-11-30 00:37:56 +0100

    optimised level save file size

--- core/game_objects/game_object.rb
+++ core/game_objects/game_object.rb
@@ -7,15 +7,18 @@ class GameObject
         'nothing_64x.png'
     end
     def initialize(id: nil, name: nil, img_path: nil, position: Vector2.new(0, 0))
-        img_path = self.class.default_texture unless img_path
-        file_path = File.join(Config::ASSETS_DIR, img_path)
+        file_path = File.join(Config::ASSETS_DIR, img_path || self.class.default_texture)
         @id, @name, @texture, @position, @img_path = id, name, Gosu::Image.new(file_path), position, img_path
     end 
     def draw x=@position.x, y=@position.y
         @texture.draw(x, y, 0)
     end
     def hash
-        {"id": @id, "type": self.class.to_s, "name": @name, "position": {"x": @position.x, "y": @position.y}, "data": {"texture": @img_path}}
+        hash = {"type": self.class.to_s, "position": {"x": @position.x, "y": @position.y}}
+        hash["id"] = @id if @id
+        hash["name"] = @name if @name
+        hash["data"] = {"texture": @img_path} if @img_path
+        hash
     end
     def self.pretty_s
         self.to_s.split('::').last

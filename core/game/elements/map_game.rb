081310e29ff038254e8575804df6a9f6420b9cdf
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-12-01 00:45:30 +0100

    WhatsArbre messages position

--- core/game/elements/map_game.rb
+++ core/game/elements/map_game.rb
@@ -3,11 +3,11 @@ require_relative './whats_arbre'
 class MapGameDisplay < GridGameContainer
     WA_MARGIN = 20
     WA_BTN_SIZE = 80
-    WA_WIDTH = 300
+    WA_WIDTH = 400
     def build
         self.background_color = Gosu::Color::rgba(0,200,0, 255)
         @sub_elements[:whats_arbre_button] = Button.new(@root, "Whats\nArbre"){Rectangle2.new(@rectangle.right - WA_BTN_SIZE - WA_MARGIN, @rectangle.bottom - WA_BTN_SIZE - WA_MARGIN, WA_BTN_SIZE, WA_BTN_SIZE)}
-        @sub_elements[:whats_arbre] = WhatsArbre.new(@root){@rectangle.relative_to(x: @rectangle.width - 300 - WA_MARGIN, y: WA_MARGIN, height: - 3*WA_MARGIN - WA_BTN_SIZE).assign!(width: WA_WIDTH)}
+        @sub_elements[:whats_arbre] = WhatsArbre.new(@root){@rectangle.relative_to(x: @rectangle.width - WA_WIDTH - WA_MARGIN, y: WA_MARGIN, height: - 3*WA_MARGIN - WA_BTN_SIZE).assign!(width: WA_WIDTH)}
         super
     end
 end
--- core/game/elements/whats_arbre.rb
+++ core/game/elements/whats_arbre.rb
@@ -1,9 +1,10 @@
 require_relative '../../ui_elements/widgets/scrollable'
 require_relative '../../ui_elements/widgets/list'
+require_relative '../../ui_elements/drawables/text'
 class WhatsArbre < UIElement
     INPUT_SECT_HEIGHT = 50
     def build
-        self.background_color = Gosu::Color::rgba(255,255,255,200)
+        self.background_color = Gosu::Color::rgba(200,200,200,128)
         @sub_elements[:scroll] = MessagesScroll.new(@root){@rectangle.relative_to(height: -INPUT_SECT_HEIGHT)}
     end
     class MessagesScroll < Scrollable
@@ -22,13 +23,22 @@ class WhatsArbre < UIElement
         LOCAL_MESSAGE_COLOR = Gosu::Color::rgba(128,255,128,255)
         FOREIGN_MESSAGE_COLOR = Gosu::Color::rgba(128,128,255,255)
         def build
-            self.background_color = FOREIGN_MESSAGE_COLOR
+            self.background_color = LOCAL_MESSAGE_COLOR
+            @sub_elements[:title] = Text.new(@root, center_text: false){@rectangle.assign(height: 20).relative_to(x:5, y:5)}
+            @sub_elements[:text] = Text.new(@root, center_text: false){@rectangle.relative_to(x:5, y:30, width: -10, height: -30)}
         end
-        def update_data data
-            puts "data :) #{data}"
+        def update_data message
+            @sub_elements[:title].string = message.source
+            @sub_elements[:text].string = message.text
+            @is_foreign = (message.source == "Robert")
+            self.background_color = (@is_foreign ? FOREIGN_MESSAGE_COLOR : LOCAL_MESSAGE_COLOR)
         end
         def list_constraint parent_rect
-            parent_rect.assign(y: 0, height: 100)
+            if @is_foreign
+                parent_rect.assign(y: 0, height: 100).relative_to!(x:5, width: -60)
+            else
+                parent_rect.assign(y: 0, height: 100).relative_to!(x:55, width: -60)
+            end
         end
     end
     class Message

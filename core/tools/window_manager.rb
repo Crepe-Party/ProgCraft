3e31f72b2bc8abf84e64ad3bf699890bbc1d5711
Diogo <diogof-6@hotmail.com>
2020-12-01 10:48:59 +0100

    var conventions

--- core/tools/window_manager.rb
+++ core/tools/window_manager.rb
@@ -3,38 +3,38 @@ include Fox
 
 class WindowManager
     def initialize
-        theApp = FXApp.new 
-        @theMainWindow = FXMainWindow.new(theApp, "Hello")
-        theApp.create
+        fx_app = FXApp.new 
+        @fx_main_window = FXMainWindow.new(fx_app, "WindowManager")
+        fx_app.create
     end
 
     def open_file initial_dir: "/", title: "Open file", patterns: ["All Files (*)"], preferred_file_filter: 0
-        openDialog = FXFileDialog.new(@theMainWindow, title)
-        openDialog.directory = initial_dir
-        openDialog.patternList = patterns
-        openDialog.currentPattern = preferred_file_filter
+        open_dialog = FXFileDialog.new(@fx_main_window, title)
+        open_dialog.directory = initial_dir
+        open_dialog.patternList = patterns
+        open_dialog.currentPattern = preferred_file_filter
         file = ""
-        if openDialog.execute != 0
-            @preferred_file_filter = openDialog.currentPattern
-            file = openDialog.filename
+        if open_dialog.execute != 0
+            @preferred_file_filter = open_dialog.currentPattern
+            file = open_dialog.filename
         end
         file
     end
 
     def save_file initial_dir: "/", title: "Save a ProgCraft map", filename: Time.now.strftime("%Y-%m-%d"), default_extension: 'json', patterns: ["All Files (*)"], preferred_file_filter: 0
-        saveDialog = FXFileDialog.new(@theMainWindow, title)
-        saveDialog.directory = initial_dir
-        saveDialog.patternList = patterns
-        saveDialog.currentPattern = @preferred_file_filter || preferred_file_filter
+        save_dialog = FXFileDialog.new(@fx_main_window, title)
+        save_dialog.directory = initial_dir
+        save_dialog.patternList = patterns
+        save_dialog.currentPattern = @preferred_file_filter || preferred_file_filter
         file = ""
-        if saveDialog.execute != 0
-            if File.exist? saveDialog.filename
-                if MBOX_CLICKED_NO == FXMessageBox.question(@theMainWindow, MBOX_YES_NO,
+        if save_dialog.execute != 0
+            if File.exist? save_dialog.filename
+                if MBOX_CLICKED_NO == FXMessageBox.question(@fx_main_window, MBOX_YES_NO,
                     "Overwrite file", "Overwrite existing file?")
                     return ""
                 end
             end            
-            file = saveDialog.filename
+            file = save_dialog.filename
         end
         return file == "" ? "" : File.extname(file) == "" ? "#{file}.#{default_extension}" : file
     end

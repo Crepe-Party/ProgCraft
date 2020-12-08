d397b9b5ce14f18c7c9a85aa99e4efa55e42c790
Nicolas Maitre <47384878+nicolas-maitre@users.noreply.github.com>
2020-12-08 10:05:53 +0100

    hide WhatsArbre

--- core/game/elements/map_game.rb
+++ core/game/elements/map_game.rb
@@ -1,16 +1,30 @@
 require_relative '../../ui_elements/widgets/grid_game_container'
 require_relative './whats_arbre'
 class MapGameDisplay < GridGameContainer
+    attr_reader :whats_arbre_open
     WA_MARGIN = 20
     WA_BTN_SIZE = 80
     WA_WIDTH = 400
     def build
-        self.background_color = Gosu::Color::rgba(0,200,0, 255)
+        @whats_arbre_open = false
+        @whats_arbre_top_fraction = 1
+        self.background_color = Gosu::Color::rgba(0,200,0,255)
+        @sub_elements[:whats_arbre] = WhatsArbre.new(@root).constrain do 
+            top_pos = @whats_arbre_top_fraction * (@rectangle.height - WA_MARGIN)
+            @rectangle.relative_to(x: @rectangle.width - WA_WIDTH - WA_MARGIN, y: WA_MARGIN + top_pos, height: - 3*WA_MARGIN - WA_BTN_SIZE).assign!(width: WA_WIDTH)
+        end
         @sub_elements[:whats_arbre_button] = Button.new(@root, background_image: 'icons/whatsapp_logo_64x.png', bg_color: Gosu::Color::rgba(255,255,255,128))
-            .constrain{Rectangle2.new(@rectangle.right - WA_BTN_SIZE - WA_MARGIN, @rectangle.bottom - WA_BTN_SIZE - WA_MARGIN, WA_BTN_SIZE, WA_BTN_SIZE)}
-            .on_click{@sub_elements[:whats_arbre].push(WhatsArbre::Message.new("Salut", ["Robert", "You"].sample))}
-
-        @sub_elements[:whats_arbre] = WhatsArbre.new(@root){@rectangle.relative_to(x: @rectangle.width - WA_WIDTH - WA_MARGIN, y: WA_MARGIN, height: - 3*WA_MARGIN - WA_BTN_SIZE).assign!(width: WA_WIDTH)}
+            .constrain{ Rectangle2.new(@rectangle.right - WA_BTN_SIZE - WA_MARGIN, @rectangle.bottom - WA_BTN_SIZE - WA_MARGIN, WA_BTN_SIZE, WA_BTN_SIZE) }
+            .on_click{ self.whats_arbre_open^=true }
         super
+    end
+    def whats_arbre_open= is_open
+        return is_open if @whats_arbre_open == is_open
+        @whats_arbre_open = is_open
+        #animate
+        from_fr = @whats_arbre_top_fraction
+        to_fr = is_open ? 0 : 1
+        @whats_arbre_top_fraction = to_fr
+        @sub_elements[:whats_arbre].apply_constraints
     end
 end
--- core/game/elements/whats_arbre.rb
+++ core/game/elements/whats_arbre.rb
@@ -10,14 +10,21 @@ class WhatsArbre < UIElement
     def build
         self.background_color = Gosu::Color::rgba(200,200,200,128)
         @sub_elements[:scroll] = MessagesScroll.new(@root){@rectangle.relative_to(height: -INPUT_SECT_HEIGHT)}
+
         @sub_elements[:clear_btn] = Button.new(@root, "Clear")
             .constrain{@rectangle.relative_to(x: 5, y: 5).assign!(width: 150, height: 50)}
             .add_event(:click){self.clear}
 
+        @sub_elements[:rand_btn] = Button.new(@root, "Random")
+            .constrain{rc = @sub_elements[:clear_btn].rectangle; rc.assign(x: rc.right + 5)}
+            .add_event(:click){self.push(WhatsArbre::Message.new("Salut", ["Robert", "You"].sample))}
+
         @sub_elements[:input_section] = Rectangle.new(@root, Gosu::Color::rgba(128,128,128,128))
             .constrain{@rectangle.relative_to(y: @rectangle.height - INPUT_SECT_HEIGHT).assign!(height: INPUT_SECT_HEIGHT)}
+
         @sub_elements[:text_input] = TextInput.new(@root, placeholder: "Your message...")
             .constrain{@sub_elements[:input_section].rectangle.relative_to(x: 5, y: 5, height: -10, width: -SEND_BTN_WIDTH - 15)}
+
         @sub_elements[:send_button] = Button.new(@root, ">")
             .constrain{rc = @sub_elements[:text_input].rectangle; rc.assign(x: rc.right + 5, width: SEND_BTN_WIDTH)}
             .on_click do
@@ -40,7 +47,7 @@ class WhatsArbre < UIElement
     #sub classes
     class MessagesScroll < Scrollable
         def build
-            @sub_elements[:list] = List.new(@root, MessageElement, spacing: 20, start_offset: 20, parent_element: self){@scrl_rect}
+            @sub_elements[:list] = List.new(@root, MessageElement, spacing: 20, start_offset: 20 + 50, parent_element: self){@scrl_rect}
             super
         end
     end

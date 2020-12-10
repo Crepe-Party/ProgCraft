94b94e720cc04c9c2841b17e2ba414c4394f9a8e
Florian Bergmann <flobergmann@aol.ch>
2020-12-10 12:00:37 +0100

    replace \ by / in path file

--- core/config.rb
+++ core/config.rb
@@ -1,5 +1,6 @@
 module Config
-    @@base_path = ENV["OCRA_EXECUTABLE"] != nil ? File.join(File.dirname(ENV["OCRA_EXECUTABLE"]),"core") : File.dirname(__FILE__)    
+    @@base_path = ENV["OCRA_EXECUTABLE"] != nil ? File.join(File.dirname(ENV["OCRA_EXECUTABLE"]),"core") : File.dirname(__FILE__)
+    @@base_path.gsub!("\\", "/")
 
     ASSETS_DIR = File.join(@@base_path, "assets")
     MY_CODES_DIR = File.join(@@base_path, "..", "mycodes")

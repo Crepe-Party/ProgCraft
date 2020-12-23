03bddeaedc3513cf12bbdac7dae996172695f6a4
Diogo <diogof-6@hotmail.com>
2020-12-23 14:32:12 +0100

    fix rotate_by_direction

--- mycodes/rotate_by_direction.rb
+++ mycodes/rotate_by_direction.rb
@@ -1,13 +1,11 @@
-turn_to("north")
-
 def turn_to(direction)
-    current_direction = compass()
-    if current_direction == direction
-        message = "You are already heading "+direction
+    if compass() == direction
+        message = "You are already heading " + direction
         say(message)
     else
-        while current_direction != direction
+        while compass() != direction
             turn_right
         end
     end
 end
+turn_to("north")

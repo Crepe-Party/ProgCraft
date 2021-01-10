1c1c0e10f20d6fef6a30f2d9519daf75f83275c1
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2021-01-10 23:47:07 +0100

    label alignment

--- core/editor/elements/contextual_menu.rb
+++ core/editor/elements/contextual_menu.rb
@@ -8,7 +8,7 @@ class EditorContextualMenu < UIElement
         @sub_elements[:map_width_label] = Text.new(@root, "Map width", center_text: :vertical)
         .constrain{@rectangle.relative_to(x:5, y:5, width: -10).assign!(height:INPUT_HEIGHT)}
         @sub_elements[:map_width_input] = TextInput.new(@root, placeholder: "Enter a width")
-        .constrain{@sub_elements[:map_width_label].rectangle.relative_to(y:INPUT_HEIGHT)}
+        .constrain{@sub_elements[:map_width_label].rectangle.relative_to(y:INPUT_HEIGHT - 5)}
         .add_event(:submit) do |value:, input:|
             val = value.to_i
             @root.level.maps.first.size.x = val if val > 0
@@ -17,9 +17,9 @@ class EditorContextualMenu < UIElement
         
         #map height
         @sub_elements[:map_height_label] = Text.new(@root, "Map height", center_text: :vertical)
-        .constrain{@sub_elements[:map_width_input].rectangle.relative_to(y: INPUT_HEIGHT + 10)}
+        .constrain{@sub_elements[:map_width_input].rectangle.relative_to(y: INPUT_HEIGHT + 5)}
         @sub_elements[:map_height_input] = TextInput.new(@root, placeholder: "Enter a height")
-        .constrain{@sub_elements[:map_height_label].rectangle.relative_to(y: INPUT_HEIGHT)}
+        .constrain{@sub_elements[:map_height_label].rectangle.relative_to(y: INPUT_HEIGHT - 5)}
         .add_event(:submit) do |value:, input:|
             val = value.to_i
             @root.level.maps.first.size.y = val if val > 0

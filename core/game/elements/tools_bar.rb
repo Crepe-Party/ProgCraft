fe7a8236807a4d92c063ce9870f127cbbc825ed1
Diogo <diogof-6@hotmail.com>
2021-01-21 22:58:12 +0100

    speed change dynamically when changing values

--- core/game/elements/tools_bar.rb
+++ core/game/elements/tools_bar.rb
@@ -3,6 +3,8 @@ require_relative '../../ui_elements/drawables/text'
 require_relative '../../ui_elements/widgets/text_input'
 class ToolsBar < UIElement    
     BAR_HEIGHT = 50
+    INPUTS_HEIGHT = BAR_HEIGHT - 20
+    SEPARATION_HEIGHT = 2
     def build
         self.background_color = Gosu::Color::rgba(64,64,64,255)
         @sub_elements[:title] = Text.new(@root, "Speed:", color: Gosu::Color::WHITE, center_text: :vertical)
@@ -10,17 +12,29 @@ class ToolsBar < UIElement
         @sub_elements[:text_input] = TextInput.new(@root, placeholder: "1",border_width: 2, cursor_width: 2)
             .constrain do
                 rect = @sub_elements[:title].rectangle 
-                rect.assign(x: rect.right, y: rect.y + 10, height: BAR_HEIGHT - 20, width: 50)
+                rect.assign(x: rect.right, y: rect.y + 10, height: INPUTS_HEIGHT, width: 50)
             end
-            .add_event(:submit) do |value:, input:|
-                @sub_elements[:text_input].text_input_unplugged
+            .add_event(:submit) {|value:, input:| @sub_elements[:text_input].text_input_unplugged} # remove focus on submit
+            .add_event(:change) do |value:, input:|
                 begin
+                    p value
                     value = Integer(value)
+                    value = -value if value < 0
                 rescue => exception
                     next
                 end
                 @root.robert.speed = value 
             end
+        
+        @sub_elements[:speed_button] = Button.new(@root, "x1")
+            .constrain do
+                rect = @sub_elements[:text_input].rectangle
+                rect.assign(x: rect.right + 10, width: 30, height: INPUTS_HEIGHT )
+            end
+
+
+        @sub_elements[:tools_bar_separation] = ToolsBarSeparation.new(@root)
+        .constrain{ rect = @rectangle; rect.assign(y:rect.bottom - SEPARATION_HEIGHT, height: SEPARATION_HEIGHT)}
     end
 end
 class ToolsBarSeparation < UIElement    

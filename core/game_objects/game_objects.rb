12b824ebace01c6a3513f9194996002cfb426229
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2021-06-26 01:40:38 +0200

    misc improvements - letter gameObject - text color for buttons - better consistency before loading map - big map (laby)

--- core/game_objects/game_objects.rb
+++ core/game_objects/game_objects.rb
@@ -43,4 +43,18 @@ module GameObjects
             "wall_64x.png"
         end
     end
+    class Letter < GameObject
+        attr_accessor :letter, :font, :color, :position
+        def initialize(position:Vector2(0,0), letter:1, font: Gosu::Font.new(64, name: Gosu::default_font_name), color: Gosu::Color::BLACK)
+            @letter = letter
+            @font = font
+            @color = color
+            @position = position
+        end 
+        def draw x = @position.x, y = @position.y
+            rendered_text = @letter.to_s
+            x += (64 - @font.text_width(rendered_text)) / 2
+            @font.draw_text(rendered_text, x, y, 0, 1, 1, @color)
+        end
+    end
 end
